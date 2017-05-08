7921e71ebec414f1072296365ed53479245c65e5
Patrick Whitehouse <patrick1996@gmail.com>
2017-05-08 15:55:04 +0100

    For loop :octocat:

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 new Vue({
   el: '#app',
   data: {
-    success: false,
-    error: false
-  },
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 },
+            {name: 'Patrick', age: 21}
+        ]
+    },
   methods:{
 
   },
