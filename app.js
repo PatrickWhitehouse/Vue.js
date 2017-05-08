e7dee97bae4094b65dec2c65cc6c7923b66d9d28
Patrick Whitehouse <patrick1996@gmail.com>
2017-05-08 15:09:41 +0100

    Keyboard listeners :sparkles:

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 new Vue({
   el: '#app',
   data: {
-    age: 21
-  },
-  methods:{
-    add: function(inc){
-      this.age+=inc;
-    },
-    subtract: function(dec){
-      this.age-=dec;
-    }
-  }
-});
 
-new Vue({
-  el: '#canvas',
-  data: {
-    x:0,
-    y:0
   },
   methods:{
-    updatexy:function(event){
-      this.x = event.offsetX;
-      this.y = event.offsetY;
+    logName: function(){
+      console.log('You entered your name');
+    },
+    logAge: function(){
+      console.log('You entered your age');
     }
   }
 });
