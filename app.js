1d6ce93f4f9057b3eaff2f17235964ff8de6931b
Patrick Whitehouse <patrick1996@gmail.com>
2017-05-08 15:38:17 +0100

    Dynamic CSS Classes :fire:

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    name: '',
-    age:''
+    available: false,
+    nearby: false
   },
   methods:{
-    
+
+  },
+  computed:{
+    compClasses: function(){
+      return{
+        available: this.available,
+        nearby: this.nearby
+      }
+    }
   }
 });
