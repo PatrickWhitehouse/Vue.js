be38a39dcc2f37cc0534e05ac7dfe83f377cfb6f
Patrick Whitehouse <patrick1996@gmail.com>
2017-05-08 20:40:01 +0100

    Vue JS using the CLI

---
+++ vuejs/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <app-header v-bind:title="title"></app-header>
+    <app-body v-bind:ninjas="ninjas"></app-body>
+    <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue';
+import Footer from './components/footer.vue';
+import Body from './components/body.vue';
+export default {
+
+  data(){
+    return{
+      ninjas: [
+              {name: 'Ryu', speciality: 'Vue Components', show: false},
+              {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+              {name: 'Hitoshi', speciality: 'Click Events', show: false},
+              {name: 'Tango', speciality: 'Conditionals', show: false},
+              {name: 'Kami', speciality: 'Webpack', show: false},
+              {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ],
+      title: "Vue JS"
+    }
+  },
+  components:{
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-body': Body
+  }
+}
+</script>
+
+<style>
+body{
+  font-family: sans-serif;
+}
+</style>
---
+++ vuejs/src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+<header>
+  <h1>{{title}}</h1>
+</header>
+</template>
+
+<script>
+export default {
+  props:{
+    title:{
+      type: String
+    }
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+header{
+  background: lightgreen;
+  padding: 10px;
+}
+h1{
+  font-size: 2rem;
+}
+
+</style>
